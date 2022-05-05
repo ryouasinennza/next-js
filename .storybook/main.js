9eb2ce70eae89cb0eba76255e7f0ba3602df7aa5
ikedahideharu <hideharu.ikeda@gmail.com>
2022-05-05 15:59:44 +0900

    @storybook/addon-postcss install

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
   framework: '@storybook/react',
   staticDirs: ['../public'],
 }
