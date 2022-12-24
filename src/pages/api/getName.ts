ec57075eab2047b52d6cf347f3b4176b019ef932
ikedahideharu <hideharu.ikeda@gmail.com>
2022-12-24 13:56:55 +0900

    chore: update (#80)

--- src/pages/api/getName.ts
+++ src/pages/api/getName.ts
@@ -1,9 +1,10 @@
+import { setTimeout } from 'node:timers/promises'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const getName = (req: NextApiRequest, res: NextApiResponse): void => {
-  setTimeout(() => {
-    res.status(200).json({ name: 'John Doe' })
-  }, 2000)
+const getName = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  await setTimeout(2000)
+  await res.status(200).json({ name: 'John Doe' })
+  res.end()
 }
 
 export default getName
